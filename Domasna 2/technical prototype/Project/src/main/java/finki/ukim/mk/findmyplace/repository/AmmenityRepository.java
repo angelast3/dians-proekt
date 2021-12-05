12ca6cd109ac3c757f90b28fc6a301dcfbd0879a
Stefan <stefan.gta@hotmail.com>
2021-12-05 16:34:09 +0100

    ameneties are read from the csvs

--- Domasna 2/technical prototype/Project/src/main/java/finki/ukim/mk/findmyplace/repository/AmmenityRepository.java
+++ Domasna 2/technical prototype/Project/src/main/java/finki/ukim/mk/findmyplace/repository/AmmenityRepository.java
@@ -4,6 +4,9 @@ import finki.ukim.mk.findmyplace.model.Ammenity;
 import finki.ukim.mk.findmyplace.model.AmmenityDoesNotExistException;
 import org.springframework.stereotype.Repository;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,9 +15,30 @@ public class AmmenityRepository {
     public static List<Ammenity> ammenityList = new ArrayList<>();
     private final CityRepository cityRepository = new CityRepository();
 
-    public AmmenityRepository() {
-        ammenityList.add(new Ammenity("Broz Cafe", "Broz Cafe", 21.416703, 42.0001541, "070000000", "MK", cityRepository.findByName("Skopje").getCityID(), "Partizanska", "24/7", "www.example.com", 0));
-        ammenityList.add(new Ammenity("Play Cafe", "Play Cafe", 21.429836, 41.992653, "070111111", "MK", cityRepository.findByName("Skopje").getCityID(), "Jane Sandanski", "Mo-Su 09:00-21:00", "www.example.com", 0));
+    public AmmenityRepository() throws IOException {
+        BufferedReader barCsvReader = new BufferedReader(new FileReader("src/main/java/finki/ukim/mk/findmyplace/repository/barDataset.csv"));
+        BufferedReader cafeCsvReader = new BufferedReader(new FileReader("src/main/java/finki/ukim/mk/findmyplace/repository/cafeDataset.csv"));
+        String row = "";
+        int flag = 0;
+        Random random = new Random();
+
+        while((row = barCsvReader.readLine())!= null){
+            String []barData = row.split(",");
+            if ( flag > 0 ){
+                ammenityList.add(new Ammenity(barData[3], barData[3], Double.parseDouble(barData[1]), Double.parseDouble(barData[2]), "07" + String.format("%07d", random.nextInt(10000000)), "MK", cityRepository.findByName("Skopje").getCityID(), "Partizanska", "09:00-01:00", "", 1));
+            }
+            flag++;
+        }
+
+        flag = 0;
+        while((row = cafeCsvReader.readLine())!= null){
+            String []cafeData = row.split(",");
+            if (flag > 0){
+                ammenityList.add(new Ammenity(cafeData[3], cafeData[3], Double.parseDouble(cafeData[1]), Double.parseDouble(cafeData[2]),"07" + String.format("%07d", random.nextInt(10000000)), "MK", cityRepository.findByName("Skopje").getCityID(), "Partizanska", "09:00-01:00", "", 0));
+            }
+            flag++;
+        }
+
     }
 
     public List<Ammenity> listAll(){
