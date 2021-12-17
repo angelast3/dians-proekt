f227c713dc6da5ec4807135e48ad4fee436770dc
Angela <angela.stevkovska3@gmail.com>
2021-12-17 01:13:11 +0100

    easier navigation and UI

--- Domasna 3/FindMyPlaceProject/src/main/java/finki/ukim/mk/findmyplace/repository/AmmenityRepository.java
+++ Domasna 3/FindMyPlaceProject/src/main/java/finki/ukim/mk/findmyplace/repository/AmmenityRepository.java
@@ -24,7 +24,7 @@ public class AmmenityRepository {
         while((row = barCsvReader.readLine())!= null){
             String []barData = row.split(",");
             if ( flag > 0 ){
-                ammenityList.add(new Ammenity(barData[0], barData[4], barData[3], Double.parseDouble(barData[1]), Double.parseDouble(barData[2]), "07" + String.format("%07d", random.nextInt(10000000)), "MK", barData.length >= 7 && !barData[6].equals("") ? barData[6] : null, barData.length >= 8 && !barData[7].equals("") ? barData[7] : null, "09:00-01:00", "", 1));
+                ammenityList.add(new Ammenity(barData[0], barData[4], barData[3], Double.parseDouble(barData[1]), Double.parseDouble(barData[2]), "07" + String.format("%07d", random.nextInt(10000000)), "MK", barData.length >= 7 && !barData[6].equals("") ? barData[6] : null, barData.length >= 8 && !barData[7].equals("") ? barData[7] : null, barData.length >= 9 && !barData[8].equals("") ? barData[8] : null, "", 1));
             }
             flag++;
         }
@@ -33,7 +33,7 @@ public class AmmenityRepository {
         while((row = cafeCsvReader.readLine())!= null){
             String []cafeData = row.split(",");
             if (flag > 0){
-                ammenityList.add(new Ammenity(cafeData[0], cafeData[4], cafeData[3], Double.parseDouble(cafeData[1]), Double.parseDouble(cafeData[2]),"07" + String.format("%07d", random.nextInt(10000000)), "MK", cafeData.length >= 7 && !cafeData[6].equals("") ? cafeData[6] : null, cafeData.length >= 8 && !cafeData[7].equals("") ? cafeData[7] : null, "09:00-01:00", "", 0));
+                ammenityList.add(new Ammenity(cafeData[0], cafeData[4], cafeData[3], Double.parseDouble(cafeData[1]), Double.parseDouble(cafeData[2]),"07" + String.format("%07d", random.nextInt(10000000)), "MK", cafeData.length >= 7 && !cafeData[6].equals("") ? cafeData[6] : null, cafeData.length >= 8 && !cafeData[7].equals("") ? cafeData[7] : null, cafeData.length >= 9 && !cafeData[8].equals("") ? cafeData[8] : null, "", 0));
             }
             flag++;
         }
