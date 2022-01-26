3dcbb05587b6c847c22fcca316caca280dd3724a
Stefan <stefan.gta@hotmail.com>
2022-01-26 19:54:41 +0100

    fix: remove comment

--- Domasna-3/Microservices/src/main/java/finki/ukim/mk/microservices/amenity/AmenityRepository.java
+++ Domasna-3/Microservices/src/main/java/finki/ukim/mk/microservices/amenity/AmenityRepository.java
@@ -1,6 +1,5 @@
 package finki.ukim.mk.microservices.amenity;
 import org.springframework.data.jpa.repository.JpaRepository;;
 
-// TODO: make sure that this works with JpaRepository, if not use: import org.springframework.data.repository.Repository;
 public interface AmenityRepository extends JpaRepository<Amenity, Long>{
 }
